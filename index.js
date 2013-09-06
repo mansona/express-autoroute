71b6fe92237edc246317c142762e85e68bf5e879
JueLance <zzc2626@163.com>
2013-09-07 00:36:45 +0800

    Update index.js
    when I used express-autoroute in Microsoft WebMatrix, the path returned from require.main.filename is not correct, so maybe we need get the path from current working directory. But I don't test it in Linux environment, I'm not sure it will also work well.

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var winston = require('winston');
 
 module.exports = function(app) {
 
-	var routesDir = path.dirname(require.main.filename) + "/routes";
-
+	//var routesDir = path.dirname(require.main.filename) + "/routes";
+	var routesDir = path.join(process.cwd(), "routes");
+	
 	winston.debug("Loading routes directory", {
 		dir: routesDir
 	});
