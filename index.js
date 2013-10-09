e54fcfcbf41dbe0691c3d5923d1128db02bdc3bf
Sam Sharp <sam@bloo.ie>
2013-10-09 10:23:10 +0000

    added comment and logging of which logger it is using

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var loader = require('./lib/loader');
 var path = require('path');
 
 module.exports = function(app,logger) {
-    if (! logger || (! (logger.debug && typeof logger.debug == 'function'))){
-        console.log('not passed a working logger. using default')
+    //is a winston instance is passed as the second argument it will be used for autoroute logging
+    //otherwise it will use a 'clean' winston instance
+    if (! logger || (! (logger.debug && typeof logger.debug == 'function'))) {
         var logger = require('winston');
+        logger.debug('using custom logger');
+    } else {
+        logger.debug('using specified logger')
     }
 
 	var routesDir = path.dirname(require.main.filename) + "/routes";
