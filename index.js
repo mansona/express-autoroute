b3fc716be4540fa627d0e36a391ae76095dd1939
Sam Sharp <sam@bloo.ie>
2013-10-10 10:34:08 +0000

    added throwErrors to options object

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var loader = require('./lib/loader');
 var path = require('path');
 
-module.exports = function(app,logger) {
+module.exports = function(app, options) {
     //is a winston instance is passed as the second argument it will be used for autoroute logging
     //otherwise it will use a 'clean' winston instance
-    if (! logger || (! (logger.debug && typeof logger.debug == 'function'))) {
-        var logger = require('winston');
-        logger.debug('using custom logger');
-    } else {
-        logger.debug('using specified logger')
+    if (!options.logger || (!(options.logger.debug && typeof options.logger.debug == 'function'))) {
+        options.logger = require('winston');
+        options.logger.debug('using custom logger');
+    }
+    else {
+        options.logger.debug('using specified logger')
+
     }
 
-	var routesDir = path.dirname(require.main.filename) + "/routes";
+    var routesDir = path.dirname(require.main.filename) + "/routes";
 
-	logger.debug("Loading routes directory", {
-		dir: routesDir
-	});
-	
-	loader.loadDirectory(routesDir, "", app,logger);
+    options.logger.debug("Loading routes directory", {
+        dir: routesDir
+    });
+
+    loader.loadDirectory(routesDir, "", app, options);
 };
