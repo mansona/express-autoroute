58807b57752d5a24287dcce979930e40a1d0c147
Chris Manson <chris@stonecircle.io>
2017-06-23 09:11:59 +0100

    fixing more style

--- routes/test.js
+++ routes/test.js
@@ -1,33 +1,33 @@
+function getTest(req, res) {
+  res.send('get');
+}
+
+function getTestParameter(req, res) {
+  res.send(req.params.id);
+}
+
+function postTest(req, res) {
+  res.send('post');
+}
+
+function postTestParameter(req, res) {
+  res.send(req.params.id);
+}
+
+function deleteTest(req, res) {
+  res.send(req.params.id);
+}
+
 module.exports.autoroute = {
-	get: {
-		'/getTest' : getTest,
-		'/getTest/:id' : getTestParameter
-	},
-	post: {
-		'/postTest' : postTest,
-		'/postTest/:id' : postTestParameter,
-	},
-	delete: {
-		'/deleteTest/:id' : deleteTest
-	}
+  get: {
+    '/getTest': getTest,
+    '/getTest/:id': getTestParameter,
+  },
+  post: {
+    '/postTest': postTest,
+    '/postTest/:id': postTestParameter,
+  },
+  delete: {
+    '/deleteTest/:id': deleteTest,
+  },
 };
-
-function getTest(req, res){
-	res.send("get");
-}
-
-function getTestParameter(req, res){
-	res.send(req.params.id);
-}
-
-function postTest(req, res){
-	res.send("post");
-}
-
-function postTestParameter(req, res){
-	res.send(req.params.id);
-}
-
-function deleteTest(req, res){
-	res.send(req.params.id);
-}
