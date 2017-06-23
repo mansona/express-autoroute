8669ea4c964b2adfd1812b58bb4b1341af598a24
Chris Manson <chris@stonecircle.io>
2017-06-23 09:09:08 +0100

    updating travis config and fixing index style

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-var loader = require('./lib/loader');
-var path = require('path');
+'use strict';
 
-module.exports = function(app, options) {
-    //is a winston instance is passed as the second argument it will be used for autoroute logging
-    //otherwise it will use a 'clean' winston instance
-    if (!options.logger || (!(options.logger.debug && typeof options.logger.debug == 'function'))) {
-        options.logger = require('winston');
-        options.logger.debug('using custom logger');
-    } else {
-        options.logger.debug('using specified logger')
-    }
+const loader = require('./lib/loader');
+const path = require('path');
 
-    if (!options.routesDir && !options.routeFile) {
-        options.routesDir = path.join(process.cwd(), "routes");
-    }
+module.exports = function (app, options) {
+    // is a winston instance is passed as the second argument it will be used for autoroute logging
+    // otherwise it will use a 'clean' winston instance
+  if (!options.logger || (!(options.logger.debug && typeof options.logger.debug === 'function'))) {
+    options.logger = require('winston');
+    options.logger.debug('using custom logger');
+  } else {
+    options.logger.debug('using specified logger');
+  }
 
-    if (options.routesDir) {
+  if (!options.routesDir && !options.routeFile) {
+    options.routesDir = path.join(process.cwd(), 'routes');
+  }
 
-        options.logger.debug("Loading routes directory", {
-            dir: options.routesDir
-        });
+  if (options.routesDir) {
+    options.logger.debug('Loading routes directory', {
+      dir: options.routesDir,
+    });
 
-        loader.loadDirectory(options.routesDir, "", app, options);
-    } else if (options.routeFile) {
-        options.logger.debug("Loading route file", {
-            file: options.routeFile
-        });
-        loader.loadFile(options.routeFile, "", app, options);
-    }
-
-
+    loader.loadDirectory(options.routesDir, '', app, options);
+  } else if (options.routeFile) {
+    options.logger.debug('Loading route file', {
+      file: options.routeFile,
+    });
+    loader.loadFile(options.routeFile, '', app, options);
+  }
 };
