e6a8241bd6a6a4984da63ecaf0639c9b394b6907
Chris Manson <chris@stonecircle.io>
2017-06-23 08:53:01 +0100

    fixing the tests

--- test/differentRoutesDir.js
+++ test/differentRoutesDir.js
@@ -1,44 +1,44 @@
-var autoroute = require('../');
+const express = require('express');
+const request = require('supertest');
 
-var express = require('express');
-var request = require('supertest');
+const autoroute = require('../');
 
-var app;
-var server;
+let app;
+let server;
 
-describe('Custom Routes folder loading', function(){
-	before(function(){
-		var path = require('path');
-		app = express();
+describe('Custom Routes folder loading', () => {
+  before(() => {
+    const path = require('path');
+    app = express();
 
-		autoroute(app, {
-			throwErrors: true,
-			routesDir: path.join(process.cwd(), "test", "routes")
-		});
+    autoroute(app, {
+      throwErrors: true,
+      routesDir: path.join(process.cwd(), 'test', 'routes'),
+    });
 
-		server = app.listen(255256);
-	});
-	after(function(done){
-		app = null;
-		server.close(done);
-	});
-	describe('test.js', function(){
-		it('should return "get" from /getTest', function(done){
-			request(app).get('/getTest').expect(200).expect("get test/routes").end(done);
-		});
-		it('should return "post" from /postTest', function(done){
-			request(app).post('/postTest').expect(200).expect("post test/routes").end(done);
-		});
+    server = app.listen(4000);
+  });
+  after((done) => {
+    app = null;
+    server.close(done);
+  });
+  describe('test.js', () => {
+    it('should return "get" from /getTest', (done) => {
+      request(app).get('/getTest').expect(200).expect('get test/routes').end(done);
+    });
+    it('should return "post" from /postTest', (done) => {
+      request(app).post('/postTest').expect(200).expect('post test/routes').end(done);
+    });
 
-		//parametrised tests
-		it('should return "378" from /getTest/378', function(done){
-			request(app).get('/getTest/378').expect(200).expect("378 test/routes").end(done);
-		});
-		it('should return "3874" from /postTest/3874', function(done){
-			request(app).post('/postTest/3874').expect(200).expect("3874 test/routes").end(done);
-		});
-		it('should return "752" from /deleteTest/752', function(done){
-			request(app).del('/deleteTest/752').expect(200).expect("752 test/routes").end(done);
-		});
-	});
+		// parametrised tests
+    it('should return "378" from /getTest/378', (done) => {
+      request(app).get('/getTest/378').expect(200).expect('378 test/routes').end(done);
+    });
+    it('should return "3874" from /postTest/3874', (done) => {
+      request(app).post('/postTest/3874').expect(200).expect('3874 test/routes').end(done);
+    });
+    it('should return "752" from /deleteTest/752', (done) => {
+      request(app).del('/deleteTest/752').expect(200).expect('752 test/routes').end(done);
+    });
+  });
 });
--- test/routes/test.js
+++ test/routes/test.js
@@ -1,33 +1,33 @@
+function getTest(req, res) {
+  res.send('get test/routes');
+}
+
+function getTestParameter(req, res) {
+  res.send(`${req.params.id} test/routes`);
+}
+
+function postTest(req, res) {
+  res.send('post test/routes');
+}
+
+function postTestParameter(req, res) {
+  res.send(`${req.params.id} test/routes`);
+}
+
+function deleteTest(req, res) {
+  res.send(`${req.params.id} test/routes`);
+}
+
 module.exports.autoroute = {
-	get: {
-		'/getTest' : getTest,
-		'/getTest/:id' : getTestParameter
-	},
-	post: {
-		'/postTest' : postTest,
-		'/postTest/:id' : postTestParameter,
-	},
-	delete: {
-		'/deleteTest/:id' : deleteTest
-	}
+  get: {
+    '/getTest': getTest,
+    '/getTest/:id': getTestParameter,
+  },
+  post: {
+    '/postTest': postTest,
+    '/postTest/:id': postTestParameter,
+  },
+  delete: {
+    '/deleteTest/:id': deleteTest,
+  },
 };
-
-function getTest(req, res){
-	res.send("get test/routes");
-}
-
-function getTestParameter(req, res){
-	res.send(req.params.id + " test/routes");
-}
-
-function postTest(req, res){
-	res.send("post test/routes");
-}
-
-function postTestParameter(req, res){
-	res.send(req.params.id + " test/routes");
-}
-
-function deleteTest(req, res){
-	res.send(req.params.id + " test/routes");
-}
